02b200c0f1cf0e94c03c3df1e5fcf6f5f1ad4790
kragh <kasperkragheliasen@gmail.com>
2020-04-02 14:06:26 +0200

    figur

--- spil/sketch.js
+++ spil/sketch.js
@@ -1,13 +1,31 @@
-function setup () {
-createCanvas(windowWidth, windowHeight);
+let player;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    player = new Player();
+}
+
+function draw() {
+    background(0);
+    player.render();
+}
+//figur 
+
+function Player() {
+    this.pos = createVector(width / 2, height / 2);
+    this.r = 30;
+    this.render = function () {
+
+        translate(this.pos.x, this.pos.y);
+        noFill();
+        stroke(255,0,0);
+        triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+
+    }
 
 }
 
-function draw(){
-background(0,0,0);
 
-fill(255);
-circle(200, 200, 50);
 
-}
+
 
