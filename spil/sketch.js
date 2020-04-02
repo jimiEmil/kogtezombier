ec6fe29e2eb858b0595d09121050b2a367fdccb1
kragh <kasperkragheliasen@gmail.com>
2020-04-02 15:10:08 +0200

    drengerøv

--- spil/sketch.js
+++ spil/sketch.js
@@ -1,5 +1,6 @@
 let player;
 
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     player = new Player();
@@ -8,24 +9,44 @@ function setup() {
 function draw() {
     background(0);
     player.render();
+    //player.turn(0.1);
 }
 //figur 
+
+function keyPressed() {
+if (keyCode == RIGHT_ARROW){
+player.turn(0.1);
+} else if (keyCode == LEFT_ARROW){
+  player.turn(-0.1);
+}
+
+}
 
 function Player() {
     this.pos = createVector(width / 2, height / 2);
     this.r = 30;
-    this.render = function () {
+    this.heading = PI / 2;
 
+
+
+    this.render = function () {
         translate(this.pos.x, this.pos.y);
+        rotate(this.heading);
         noFill();
-        stroke(255,0,0);
+        stroke(255, 0, 0);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
 
+
+
+    }
+
+    this.turn = function (angle) {
+        this.heading += angle;
     }
 
 }
 
+//function keyPressed() {
 
-
-
-
+   // if (key == ' ') {
+   //   skyd.play();
